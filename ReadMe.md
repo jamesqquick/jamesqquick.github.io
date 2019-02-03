db432ddd0b8f273f6841120fd66638f3d6b8bd61
James Quick <james.q.quick@outlook.com>
2019-02-03 10:21:15 -0600

    Added most recent changes

--- ReadMe.md
+++ ReadMe.md
@@ -5,3 +5,23 @@ ES6 Features (arrow functions, spread operator, const and let)
 JavaScript - array functions (splice, map, sort), localStorage, fetch API
 
 CSS - flexbox, transitions,
+
+- Intro (who am i, learn build teach, resources, what are we going to build, etc.)
+- 1. Scaffold/Style Home Page (index.html with anchor tags, start css - text, buttons, use rems, utility classes)
+- 2. Create and Scaffold Game Page (navigate from home and scaffold html, Style Static question/answer content)
+- 3. Load questions from hard coded array (handle changing of questions)
+- 4. Animations for Correct/Incorrect Answers on Click
+- 5. Create HUD (Track question number, and score)
+- 6. Create a Progress Bar
+- 7. Create/Scaffold End Screen Page and Navigate on Click (Save High Scores in Local Storage )
+- 8. Create High Scores Page (load from local storage)
+- 9. Fetch Questions from Local JSON file (can we do import)
+- 10. Fetch API Questions from OpenDB API
+- 11. Adding a Loader
+- 12. Closing
+
+Resources
+
+- https://opentdb.com/
+- https://medium.com/code-better/css-units-for-font-size-px-em-rem-79f7e592bb97
+-
--- game.js
+++ game.js
@@ -3,7 +3,12 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const scoreText = document.getElementById("score");
 const questionCounterText = document.getElementById("questionCounter");
-
+const loader = document.getElementById("loader");
+const gameScreen = document.getElementById("game");
+const progressText = document.getElementById("progressText");
+const progressBarFull = document.querySelector(
+  "#progress-bar > .progress-bar-full"
+);
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
@@ -12,58 +17,100 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-//TODO: load form json file
-let questions = [
-  {
-    question: "What is the answer to question 1?",
-    choice1: "Choice Choice 1",
-    choice2: "Choice Choice 2",
-    choice3: "Choice Choice 3",
-    choice4: "Choice Choice 4",
-    answer: 1
-  },
-  {
-    question: "What is the answer to question 2?",
-    choice1: "Choice Choice 1",
-    choice2: "Choice Choice 2",
-    choice3: "Choice Choice 3",
-    choice4: "Choice Choice 4",
-    answer: 2
-  },
-  {
-    question: "What is the answer to question 3?",
-    choice1: "Choice Choice 1",
-    choice2: "Choice Choice 2",
-    choice3: "Choice Choice 3",
-    choice4: "Choice Choice 4",
-    answer: 4
-  }
-];
+
+//questions hard coded
+
+//questions loaded from js into script tag - global questions variable
+
+//load questions from json file
+let questions = [];
+fetch("questions.json")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(loadedQuestions) {
+    // questions = loadedQuestions;
+    // startGame();
+  })
+  .catch(err => {
+    console.error("Something bad happened", err);
+  });
+
+//load questions via api call - https://opentdb.com/api_config.php
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
+)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(loadedQuestions) {
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      //inject correct answer into random position
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+      return formattedQuestion;
+    });
+    console.log(questions);
+    //questions = loadedQuestions;
+    startGame();
+  })
+  .catch(err => {
+    console.error("Something bad happened, getting backup questions", err);
+    fetch("questions.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(loadedQuestions) {
+        questions = loadedQuestions;
+        startGame();
+      })
+      .catch(err => {
+        console.error("Something bad happened", err);
+      });
+  });
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+  //hide loader and show the game
+  loader.classList.add("hidden");
+  gameScreen.classList.remove("hidden");
 };
 
 getNewQuestion = () => {
-  if (availableQuestions.length === 0) {
-    //save score in localstorage for end screen to use
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //THE GAME IS OVER - save score in localstorage for end screen to use
     localStorage.setItem("mostRecentScore", score);
     //Go to the end page
-    window.location.assign("/end.html");
+    return window.location.assign("/end.html");
   }
+
+  //Increment question counter, question number text, and progress bar
   questionCounter++;
-  questionCounterText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = `Question: ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  //questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerHTML = currentQuestion.question;
 
   choices.forEach(choice => {
     const number = choice.dataset["number"];
-    console.log("setting choice");
-    choice.innerHTML = currentQuestion["choice" + number];
+    choice.innerText = currentQuestion["choice" + number];
   });
 
   //Remove question from available questions
@@ -101,7 +148,5 @@ choices.forEach(choice => {
 
 incrementScore = num => {
   score += num;
-  scoreText.innerHTML = "Score: " + score;
+  scoreText.innerHTML = `${score}`;
 };
-
-startGame();
